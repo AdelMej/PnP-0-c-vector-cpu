8818e14555a3daf431dc883c345325338882c304
AdelMej <adelmejrissi@gmail.com>
2025-09-12 15:21:19 +0200

    feat: changed the doubling test now uses a loop

--- tests/test_vector_push.c
+++ tests/test_vector_push.c
@@ -33,24 +33,22 @@ void test_vector_push_negative(void) {
 }
 
 void test_vector_push_doubling(void) {
-  vector_data_t d = 2.56;
+  size_t num_pushes = 10000;
+  vector_data_t value;
 
-  vector_push(test, d);
-  TEST_ASSERT_EQUAL_DOUBLE(2.56, test->data[0]);
-  TEST_ASSERT_EQUAL_UINT(1, test->size);
-  TEST_ASSERT_EQUAL_UINT(1, test->capacity);
+  for (size_t i = 0; i < num_pushes; i++) {
+    value = (vector_data_t)(i * 1.1); // example values: 0.0, 1.1, 2.2, ...
+    vector_push(test, value);
 
-  d = 3.2;
-  vector_push(test, d);
-  TEST_ASSERT_EQUAL_DOUBLE(3.2, test->data[1]);
-  TEST_ASSERT_EQUAL_UINT(2, test->size);
-  TEST_ASSERT_EQUAL_UINT(2, test->capacity);
+    // Check that the value was inserted correctly
+    TEST_ASSERT_EQUAL_DOUBLE(value, test->data[i]);
 
-  d = 4.2;
-  vector_push(test, d);
-  TEST_ASSERT_EQUAL_DOUBLE(4.2, test->data[2]);
-  TEST_ASSERT_EQUAL_UINT(3, test->size);
-  TEST_ASSERT_EQUAL_UINT(4, test->capacity);
+    // Check that size is correct
+    TEST_ASSERT_EQUAL_UINT(i + 1, test->size);
+
+    // Check that capacity is enough (>= size)
+    TEST_ASSERT_TRUE(test->capacity >= test->size);
+  }
 }
 
 void test_vector_push_null(void) {
